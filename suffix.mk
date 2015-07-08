da3bc7282c9d09bc97291e0f0a53b1a8231555b0
Jim Lawson <ucbjrl@berkeley.edu>
2015-07-08 11:41:12 -0700

    Disable builtin rules; fix .out (testing) recipe; don't clean everything; try silent recipe for last resort target.

--- suffix.mk
+++ suffix.mk
@@ -18,17 +18,25 @@ firrtl	?= $(bindir)/firrtl
 genharness ?= $(bindir)/gen-harness
 flollvm ?= flo-llvm
 
-clang	?= g++
+clang	?= clang++
+
+# Don't use any built-in rules.
+MAKEFLAGS += --no-builtin-rules
+.SUFFIXES:
 
 # Don't expect reasonable output from all *.stanza files.
 # Currently, all .stanza files are okay so we use a dummy 'none'.
 units := $(filter-out none,\
             $(notdir $(basename $(wildcard $(srcdir)/*.stanza))))
 
+.PHONY:	$(units)
+
 executables := $(addprefix $(objdir)/,$(units))
 sos := $(addsuffix .so,$(addprefix $(objdir)/lib,$(units)))
 
-$(info $(sos))
+generated_suffixes := .fir .flo .h .o .out
+generated_suffixes_lib := .cpp .o .so
+generated_files := $(foreach sfx,$(generated_suffixes),$(addsuffix $(sfx),$(units))) $(units) $(foreach sfx,$(generated_suffixes_lib),$(addsuffix $(sfx),$(addprefix lib,$(units))))
 
 # Generate the rule to make something in the object directory
 
@@ -40,7 +48,7 @@ default: all
 all: outs
 
 clean:
-	cd $(top_srcdir) && rm -rf $(objdir)
+	cd $(objdir) && $(RM) $(generated_files)
 
 firs: $(tut_firs)
 
@@ -48,8 +56,8 @@ outs: $(tut_outs)
 
 verilog: $(addsuffix .v, $(executables))
 
-$(objdir)/%.out: $(objdir)/% $(objdir)/%.fir
-	echo $(chipper) or something
+$(objdir)/%.out: $(objdir)/%
+	set -e -o pipefail; cd $(objdir) && $(<F) --testing | tee $(@F)
 
 $(objdir)/lib%.so:	$(objdir)/lib%.o $(objdir)/%.o
 	cd $(objdir) && $(clang) -shared -o $(@F) -fPIC $(<F) `echo $(basename $(@F)) | sed -e s/lib//`.o
@@ -80,8 +88,10 @@ compile smoke: firs
 
 # Last resort target. The first dependency will be the object directory,
 # but only if it doesn't already exist.
+# The dummy recipe seems to be required for Ubuntu versions of Make,
+# otherwise it complains about not being able to make the target.
 %: $(filter-out $(wildcard $objdir),$(objdir)) $(objdir)/%
-	echo tried to make $@
+	@/bin/true
 
 .PRECIOUS:	$(executables) $(sos)
 
